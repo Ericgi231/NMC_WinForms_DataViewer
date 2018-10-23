314435a1c6819713c644bf872dbb64c50bb6c88b
Eric Grant <ericgi231@gmail.com>
2018-10-23 17:21:11 -0400

    created forms

--- WinForms_DataViewer/WinForms_DataViewer/DetailForm.cs
+++ WinForms_DataViewer/WinForms_DataViewer/DetailForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinForms_DataViewer
 {
-    public partial class Form1 : Form
+    public partial class DetailForm : Form
     {
-        public Form1()
+        public DetailForm()
         {
             InitializeComponent();
         }
