f085a384a7e18985a4b49e184d1728bcf676e110
Eric Grant <ericgi231@gmail.com>
2018-10-27 16:54:14 -0400

    fin

--- WinForms_DataViewer/WinForms_DataViewer/ListForm.Designer.cs
+++ WinForms_DataViewer/WinForms_DataViewer/ListForm.Designer.cs
@@ -38,10 +38,22 @@
             this.charactersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dataviewerDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.charactersBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lbl_Search = new System.Windows.Forms.Label();
+            this.lbl_Sort = new System.Windows.Forms.Label();
+            this.lbl_Filter = new System.Windows.Forms.Label();
+            this.tb_SearchValue = new System.Windows.Forms.TextBox();
+            this.cb_SearchColumn = new System.Windows.Forms.ComboBox();
+            this.cb_SortColumn = new System.Windows.Forms.ComboBox();
+            this.cb_Order = new System.Windows.Forms.ComboBox();
+            this.pan_Filter = new System.Windows.Forms.Panel();
+            this.rb_All = new System.Windows.Forms.RadioButton();
+            this.rb_Male = new System.Windows.Forms.RadioButton();
+            this.rb_Female = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_CharacterTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.charactersBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataviewerDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.charactersBindingSource1)).BeginInit();
+            this.pan_Filter.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgv_CharacterTable
@@ -49,14 +61,14 @@
             this.dgv_CharacterTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_CharacterTable.Location = new System.Drawing.Point(12, 12);
             this.dgv_CharacterTable.Name = "dgv_CharacterTable";
-            this.dgv_CharacterTable.Size = new System.Drawing.Size(666, 402);
+            this.dgv_CharacterTable.Size = new System.Drawing.Size(661, 396);
             this.dgv_CharacterTable.TabIndex = 0;
             // 
             // butt_View
             // 
-            this.butt_View.Location = new System.Drawing.Point(12, 420);
+            this.butt_View.Location = new System.Drawing.Point(679, 12);
             this.butt_View.Name = "butt_View";
-            this.butt_View.Size = new System.Drawing.Size(91, 33);
+            this.butt_View.Size = new System.Drawing.Size(92, 33);
             this.butt_View.TabIndex = 1;
             this.butt_View.Text = "View";
             this.butt_View.UseVisualStyleBackColor = true;
@@ -64,9 +76,9 @@
             // 
             // butt_Delete
             // 
-            this.butt_Delete.Location = new System.Drawing.Point(109, 420);
+            this.butt_Delete.Location = new System.Drawing.Point(679, 51);
             this.butt_Delete.Name = "butt_Delete";
-            this.butt_Delete.Size = new System.Drawing.Size(91, 33);
+            this.butt_Delete.Size = new System.Drawing.Size(92, 33);
             this.butt_Delete.TabIndex = 2;
             this.butt_Delete.Text = "Delete";
             this.butt_Delete.UseVisualStyleBackColor = true;
@@ -74,9 +86,9 @@
             // 
             // butt_Exit
             // 
-            this.butt_Exit.Location = new System.Drawing.Point(731, 420);
+            this.butt_Exit.Location = new System.Drawing.Point(730, 375);
             this.butt_Exit.Name = "butt_Exit";
-            this.butt_Exit.Size = new System.Drawing.Size(91, 33);
+            this.butt_Exit.Size = new System.Drawing.Size(41, 33);
             this.butt_Exit.TabIndex = 3;
             this.butt_Exit.Text = "Exit";
             this.butt_Exit.UseVisualStyleBackColor = true;
@@ -84,9 +96,9 @@
             // 
             // butt_Help
             // 
-            this.butt_Help.Location = new System.Drawing.Point(634, 420);
+            this.butt_Help.Location = new System.Drawing.Point(679, 375);
             this.butt_Help.Name = "butt_Help";
-            this.butt_Help.Size = new System.Drawing.Size(91, 33);
+            this.butt_Help.Size = new System.Drawing.Size(45, 33);
             this.butt_Help.TabIndex = 4;
             this.butt_Help.Text = "Help";
             this.butt_Help.UseVisualStyleBackColor = true;
@@ -94,19 +106,139 @@
             // 
             // butt_Save
             // 
-            this.butt_Save.Location = new System.Drawing.Point(380, 420);
+            this.butt_Save.Location = new System.Drawing.Point(679, 340);
             this.butt_Save.Name = "butt_Save";
-            this.butt_Save.Size = new System.Drawing.Size(91, 33);
+            this.butt_Save.Size = new System.Drawing.Size(92, 29);
             this.butt_Save.TabIndex = 5;
             this.butt_Save.Text = "Save";
             this.butt_Save.UseVisualStyleBackColor = true;
             this.butt_Save.Click += new System.EventHandler(this.butt_Save_Click);
             // 
+            // lbl_Search
+            // 
+            this.lbl_Search.AutoSize = true;
+            this.lbl_Search.Location = new System.Drawing.Point(676, 101);
+            this.lbl_Search.Name = "lbl_Search";
+            this.lbl_Search.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Search.TabIndex = 6;
+            this.lbl_Search.Text = "Search";
+            // 
+            // lbl_Sort
+            // 
+            this.lbl_Sort.AutoSize = true;
+            this.lbl_Sort.Location = new System.Drawing.Point(679, 255);
+            this.lbl_Sort.Name = "lbl_Sort";
+            this.lbl_Sort.Size = new System.Drawing.Size(26, 13);
+            this.lbl_Sort.TabIndex = 7;
+            this.lbl_Sort.Text = "Sort";
+            // 
+            // lbl_Filter
+            // 
+            this.lbl_Filter.AutoSize = true;
+            this.lbl_Filter.Location = new System.Drawing.Point(679, 167);
+            this.lbl_Filter.Name = "lbl_Filter";
+            this.lbl_Filter.Size = new System.Drawing.Size(29, 13);
+            this.lbl_Filter.TabIndex = 8;
+            this.lbl_Filter.Text = "Filter";
+            // 
+            // tb_SearchValue
+            // 
+            this.tb_SearchValue.Location = new System.Drawing.Point(679, 144);
+            this.tb_SearchValue.Name = "tb_SearchValue";
+            this.tb_SearchValue.Size = new System.Drawing.Size(92, 20);
+            this.tb_SearchValue.TabIndex = 9;
+            this.tb_SearchValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_Search_KeyPress);
+            // 
+            // cb_SearchColumn
+            // 
+            this.cb_SearchColumn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_SearchColumn.FormattingEnabled = true;
+            this.cb_SearchColumn.Location = new System.Drawing.Point(679, 117);
+            this.cb_SearchColumn.Name = "cb_SearchColumn";
+            this.cb_SearchColumn.Size = new System.Drawing.Size(92, 21);
+            this.cb_SearchColumn.TabIndex = 10;
+            // 
+            // cb_SortColumn
+            // 
+            this.cb_SortColumn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_SortColumn.FormattingEnabled = true;
+            this.cb_SortColumn.Location = new System.Drawing.Point(679, 271);
+            this.cb_SortColumn.Name = "cb_SortColumn";
+            this.cb_SortColumn.Size = new System.Drawing.Size(92, 21);
+            this.cb_SortColumn.TabIndex = 11;
+            this.cb_SortColumn.TextChanged += new System.EventHandler(this.cb_SortColumn_TextChanged);
+            // 
+            // cb_Order
+            // 
+            this.cb_Order.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Order.FormattingEnabled = true;
+            this.cb_Order.Items.AddRange(new object[] {
+            "Ascending",
+            "Descending"});
+            this.cb_Order.Location = new System.Drawing.Point(679, 298);
+            this.cb_Order.Name = "cb_Order";
+            this.cb_Order.Size = new System.Drawing.Size(92, 21);
+            this.cb_Order.TabIndex = 14;
+            this.cb_Order.TextChanged += new System.EventHandler(this.cb_Order_TextChanged);
+            // 
+            // pan_Filter
+            // 
+            this.pan_Filter.Controls.Add(this.rb_Female);
+            this.pan_Filter.Controls.Add(this.rb_Male);
+            this.pan_Filter.Controls.Add(this.rb_All);
+            this.pan_Filter.Location = new System.Drawing.Point(679, 184);
+            this.pan_Filter.Name = "pan_Filter";
+            this.pan_Filter.Size = new System.Drawing.Size(92, 68);
+            this.pan_Filter.TabIndex = 15;
+            // 
+            // rb_All
+            // 
+            this.rb_All.AutoSize = true;
+            this.rb_All.Checked = true;
+            this.rb_All.Location = new System.Drawing.Point(3, 3);
+            this.rb_All.Name = "rb_All";
+            this.rb_All.Size = new System.Drawing.Size(36, 17);
+            this.rb_All.TabIndex = 0;
+            this.rb_All.TabStop = true;
+            this.rb_All.Text = "All";
+            this.rb_All.UseVisualStyleBackColor = true;
+            this.rb_All.CheckedChanged += new System.EventHandler(this.rb_All_CheckedChanged);
+            // 
+            // rb_Male
+            // 
+            this.rb_Male.AutoSize = true;
+            this.rb_Male.Location = new System.Drawing.Point(3, 26);
+            this.rb_Male.Name = "rb_Male";
+            this.rb_Male.Size = new System.Drawing.Size(48, 17);
+            this.rb_Male.TabIndex = 1;
+            this.rb_Male.Text = "Male";
+            this.rb_Male.UseVisualStyleBackColor = true;
+            this.rb_Male.CheckedChanged += new System.EventHandler(this.rb_Male_CheckedChanged);
+            // 
+            // rb_Female
+            // 
+            this.rb_Female.AutoSize = true;
+            this.rb_Female.Location = new System.Drawing.Point(3, 49);
+            this.rb_Female.Name = "rb_Female";
+            this.rb_Female.Size = new System.Drawing.Size(59, 17);
+            this.rb_Female.TabIndex = 2;
+            this.rb_Female.Text = "Female";
+            this.rb_Female.UseVisualStyleBackColor = true;
+            this.rb_Female.CheckedChanged += new System.EventHandler(this.rb_Female_CheckedChanged);
+            // 
             // ListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 465);
+            this.ClientSize = new System.Drawing.Size(783, 420);
+            this.Controls.Add(this.pan_Filter);
+            this.Controls.Add(this.cb_Order);
+            this.Controls.Add(this.cb_SortColumn);
+            this.Controls.Add(this.cb_SearchColumn);
+            this.Controls.Add(this.tb_SearchValue);
+            this.Controls.Add(this.lbl_Filter);
+            this.Controls.Add(this.lbl_Sort);
+            this.Controls.Add(this.lbl_Search);
             this.Controls.Add(this.butt_Save);
             this.Controls.Add(this.butt_Help);
             this.Controls.Add(this.butt_Exit);
@@ -120,7 +252,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.charactersBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataviewerDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.charactersBindingSource1)).EndInit();
+            this.pan_Filter.ResumeLayout(false);
+            this.pan_Filter.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,6 +269,17 @@
         private System.Windows.Forms.Button butt_Exit;
         private System.Windows.Forms.Button butt_Help;
         private System.Windows.Forms.Button butt_Save;
+        private System.Windows.Forms.Label lbl_Search;
+        private System.Windows.Forms.Label lbl_Sort;
+        private System.Windows.Forms.Label lbl_Filter;
+        private System.Windows.Forms.TextBox tb_SearchValue;
+        private System.Windows.Forms.ComboBox cb_SearchColumn;
+        private System.Windows.Forms.ComboBox cb_SortColumn;
+        private System.Windows.Forms.ComboBox cb_Order;
+        private System.Windows.Forms.Panel pan_Filter;
+        private System.Windows.Forms.RadioButton rb_Female;
+        private System.Windows.Forms.RadioButton rb_Male;
+        private System.Windows.Forms.RadioButton rb_All;
     }
 }
 
--- WinForms_DataViewer/WinForms_DataViewer/ListForm.cs
+++ WinForms_DataViewer/WinForms_DataViewer/ListForm.cs
@@ -27,11 +27,58 @@ namespace WinForms_DataViewer
             try
             {
                 ReadMongoAndBindToDataGrid();
+                SetComboBox();
+                SetSort();
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"{ex}","Error");
             }
+        }
+
+        private void SetSort()
+        {
+            List<DataGridViewRow> sorted;
+            BindingList<Character> bindingList;
+            BindingSource source;
+            switch (cb_Order.Text)
+            {
+                case "Ascending":
+                    sorted = (from item in dgv_CharacterTable.Rows.Cast<DataGridViewRow>()
+                              where item.Index != dgv_CharacterTable.Rows.Count - 1
+                              orderby item.Cells[cb_SortColumn.Text].Value ascending
+                              select item).ToList<DataGridViewRow>();
+                    characters = sorted.Select(x => x.DataBoundItem).Cast<Character>().ToList();
+                    bindingList = new BindingList<Character>(characters);
+                    source = new BindingSource(bindingList, null);
+                    dgv_CharacterTable.DataSource = source;
+                    break;
+                case "Descending":
+                    sorted = (from item in dgv_CharacterTable.Rows.Cast<DataGridViewRow>()
+                              where item.Index != dgv_CharacterTable.Rows.Count-1
+                              orderby item.Cells[cb_SortColumn.Text].Value descending
+                              select item).ToList<DataGridViewRow>();
+                    characters = sorted.Select(x => x.DataBoundItem).Cast<Character>().ToList();
+                    bindingList = new BindingList<Character>(characters);
+                    source = new BindingSource(bindingList, null);
+                    dgv_CharacterTable.DataSource = source;
+                    break;
+            }
+
+            //dgv_CharacterTable.DataSource = sorted.Select(x => x.DataBoundItem).Cast<Character>().ToList();
+        }
+
+        private void SetComboBox()
+        {
+            foreach (DataGridViewColumn column in dgv_CharacterTable.Columns)
+            {
+                cb_SearchColumn.Items.Add(column.Name);
+                cb_SortColumn.Items.Add(column.Name);
+            }
+            cb_SearchColumn.SelectedIndex = 0;
+            cb_SortColumn.SelectedIndex = 0;
+
+            cb_Order.SelectedIndex = 0;
         }
 
         private void ReadMongoAndBindToDataGrid()
@@ -48,7 +95,6 @@ namespace WinForms_DataViewer
             var bindingList = new BindingList<Character>(characters);
             var source = new BindingSource(bindingList, null);
             dgv_CharacterTable.DataSource = source;
-
         }
 
         private void butt_Exit_Click(object sender, EventArgs e)
@@ -109,6 +155,100 @@ namespace WinForms_DataViewer
             catch (Exception ex)
             {
                 MessageBox.Show($"{ex}", "Error");
+            }
+        }
+
+        private void tb_Search_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                dgv_CharacterTable.ClearSelection();
+                int colIndex = 0;
+                foreach (DataGridViewColumn column in dgv_CharacterTable.Columns)
+                {
+                    if (column.Name == cb_SearchColumn.Text)
+                    {
+                        colIndex = column.Index;
+                    }
+                }
+
+                foreach (DataGridViewRow row in dgv_CharacterTable.Rows)
+                {
+                    try
+                    {
+                        if (row.Cells[colIndex].Value.ToString().ToLower() == tb_SearchValue.Text.ToLower())
+                        {
+                            row.Selected = true;
+                        }
+                    }
+                    catch (Exception) { }
+                }
+
+                if (dgv_CharacterTable.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Value not found.", "Error");
+                }
+            }
+        }
+
+        private void cb_Order_TextChanged(object sender, EventArgs e)
+        {
+            SetSort();
+        }
+
+        private void cb_SortColumn_TextChanged(object sender, EventArgs e)
+        {
+            SetSort();
+        }
+
+        private void tb_FilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                
+            }
+        }
+
+        private void rb_All_CheckedChanged(object sender, EventArgs e)
+        {
+            SetFilter();
+        }
+
+        private void rb_Male_CheckedChanged(object sender, EventArgs e)
+        {
+            SetFilter();
+        }
+
+        private void rb_Female_CheckedChanged(object sender, EventArgs e)
+        {
+            SetFilter();
+        }
+
+        private void SetFilter()
+        {
+            string selected = pan_Filter.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Text;
+            dgv_CharacterTable.ClearSelection();
+            foreach (DataGridViewRow row in dgv_CharacterTable.Rows)
+            {
+                if (selected != "All")
+                {
+                    try
+                    {
+                        if (row.Cells["Gender"].Value.ToString().ToLower() == selected.ToLower())
+                        {
+                            row.Visible = true;
+                        }
+                        else
+                        {
+                            row.Visible = false;
+                        }
+                    }
+                    catch (Exception) { }
+                }
+                else
+                {
+                    row.Visible = true;
+                }
             }
         }
     }
